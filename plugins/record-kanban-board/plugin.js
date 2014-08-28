587e7fee92b40a235bdf66206f127686e7fa14de
A Parks <parksa@seas.upenn.edu>
2014-08-28 17:45:59 -0400

    Update plugin.js

--- plugins/record-kanban-board/plugin.js
+++ plugins/record-kanban-board/plugin.js
@@ -1,16 +1,16 @@
 /**
- * Record Board Controller
+ * Plugin Record Board Controller
  */
 plugin.controller('recordBoardCntl', ['$scope', '$routeParams', 'znData', 'znMessage', function ($scope, $routeParams, znData, znMessage) {
 
 	// Current Workspace ID from Route
 	$scope.workspaceId = null;
 
-	// Workspace Forms
-	$scope.forms = [];
-
 	// Selected Form ID
 	$scope.formId = null;
+
+	// Workspace Forms
+	$scope.forms = [];
 
 	// Selected Form Folders
 	$scope.folders = [];
@@ -18,45 +18,38 @@ plugin.controller('recordBoardCntl', ['$scope', '$routeParams', 'znData', 'znMes
 	// Records Indexed by Folder
 	$scope.folderRecords = {};
 
+	// Show Add Folder Flag
+	$scope.showAddFolder = false;
+
 	// Add Folder Name
 	$scope.addFolderName = null;
 
+	// Selected Folder to Edit
+	$scope.editFolder = {
+		id: null,
+		name: null
+	};
+
 	// Sortable Options
 	$scope.sortableOptions = {
-		connectWith: 'ul.record-list',
-		items: 'li.record',
-		update: function(event, ui) {
-
-			// Ignore Reorder
-			if (!ui.sender) {
-					return;
-			}
+		connectWith: "ul.records-container",
+		items: "li.record",
+		stop: function(event, ui) {
 
 			// Traverse Records by Folder
 			angular.forEach($scope.folders, function(folder) {
 				angular.forEach($scope.folderRecords[folder.id], function(record, index) {
-					// Record Found
-					if (record.id == ui.item.data('id')) {
+					// Record Found and Folder Changed
+					if (record.id == ui.item.data('id') &&
+						record.folder.id != folder.id) {
 
 						// Update Record Folder ID
-						znData('FormRecords').save(
-							{
-								formId: $scope.formId,
-								id: record.id
-							},
-							{
-								folder: { id: folder.id }
-							},
-							function(response) {
-								// Update Folder Records with Response
-								$scope.folderRecords[folder.id].splice(index, 1, response);
-
-								znMessage('Record moved', 'saved');
-							},
-							function(e) {
-								znMessage('Error moving record', 'error');
-							}
-						);
+						znData('FormRecords').save({ formId: $scope.formId, id: record.id}, { folder: { id: folder.id }}, function(response) {
+							// Update Folder Records with Response
+							$scope.folderRecords[folder.id].splice(index, 1, response);
+						}, function(e) {
+							znMessage('Error moving record', 'error');
+						});
 					}
 				});
 			});
@@ -71,40 +64,10 @@ plugin.controller('recordBoardCntl', ['$scope', '$routeParams', 'znData', 'znMes
 		$scope.forms = [];
 
 		// Query Forms by Workspae ID and Return Loading Promise
-		return znData('Forms').query(
-			{
-				workspace: {
-					id: $scope.workspaceId
-				},
-				related: 'folders'
-			},
-			function(response){
-				// Set Workspace Forms from Response
-				$scope.forms = response;
-			}
-		);
-	};
-
-	/**
-	 * Pick Selected Form
-	 */
-	$scope.pickForm = function(formId) {
-		// Set Selected Form ID
-		$scope.formId = formId;
-
-		// Reset Form Folders
-		$scope.folders = [];
-
-		// Find Form and Set Selected Form Folders
-		angular.forEach($scope.forms, function(form) {
-			if (form.id == formId) {
-				$scope.folders = form.folders;
-			}
+		return znData('Forms').query({workspace: { id: $scope.workspaceId }, related: 'folders'}).then(function(response){
+			// Set Workspace Forms from Response
+			$scope.forms = response;
 		});
-
-		// Load Records for Selected Form Folders
-		$scope.loadRecords();
-
 	};
 
 	/**
@@ -122,19 +85,42 @@ plugin.controller('recordBoardCntl', ['$scope', '$routeParams', 'znData', 'znMes
 			$scope.folderRecords[folder.id] = [];
 
 			// Query and Index Records by Folder
-			var request = znData('FormRecords').query(
-				{
-					formId: $scope.formId,
-					folder: { id: folder.id }
-				},
-				function(response) {
-					$scope.folderRecords[folder.id] = response;
-				}
-			);
+			var request = znData('FormRecords').query({formId: $scope.formId, folder: { id: folder.id }}).then(function(response){
+				$scope.folderRecords[folder.id] = response;
+			});
 
 			queue.push(request);
 		});
 
+	};
+
+	/**
+	 * Pick Selected Form
+	 */
+	$scope.pickForm = function(formId) {
+		// Reset Form Folders
+		$scope.folders = [];
+
+		// Set Selected Form ID
+		$scope.formId = formId;
+
+		// Find Form and Set Selected Form Folders
+		angular.forEach($scope.forms, function(form) {
+			if (form.id == formId) {
+				$scope.folders = form.folders;
+			}
+		});
+
+		// Load Records for Selected Form Folders
+		$scope.loadRecords();
+
+	};
+
+	/**
+	 * Open or Close Add Folder Column
+	 */
+	$scope.openAddFolder = function(show) {
+		$scope.showAddFolder = show;
 	};
 
 	/**
@@ -148,36 +134,90 @@ plugin.controller('recordBoardCntl', ['$scope', '$routeParams', 'znData', 'znMes
 			}
 		};
 
-		// Reset Folder Name
-		$scope.addFolderName = '';
-
 		// Save New Folder
 		return znData('FormFolders').save({formId: $scope.formId}, data, function (folder) {
+			// Close Add Column
+			$scope.openAddFolder(false);
+
 			// Initialize New Folder Record List
 			$scope.folderRecords[folder.id] = [];
 
 			// Append New Folder to Folders List
 			$scope.folders.push(folder);
 
-			znMessage('New folder created', 'saved');
-
 			return folder;
 		}, function (e) {
-			 znMessage('Error creating folder', 'error');
+			znMessage('Error creating folder', 'error');
 		});
+	};
+
+	/**
+	 * Toggle Edit Folder
+	 */
+	$scope.toggleEditFolder = function(folderId) {
+		if ($scope.editFolder.id == folderId) {
+			// Close Edit Folder
+			$scope.editFolder.id = null;
+			$scope.editFolder.name = null;
+		}
+		else {
+			// Open Edit Folder for Folder ID
+			$scope.editFolder.id = folderId;
+
+			// Find Folder Name by ID
+			angular.forEach($scope.folders, function(folder)  {
+				if (folder.id == folderId) {
+					$scope.editFolder.name = folder.name;
+				}
+			});
+		}
+	};
+
+	/**
+	 * Save Edit Folder
+	 */
+	$scope.saveFolder = function() {
+
+		var data = {
+			name: $scope.editFolder.name,
+			form: {
+				id: $scope.formId
+			}
+		};
+
+		// Save Folder
+		return znData('FormFolders').save({formId: $scope.formId, id: $scope.editFolder.id}, data, function (response) {
+			// Update Folder in Folders List
+			angular.forEach($scope.folders, function(folder, index)  {
+				if (folder.id == $scope.editFolder.id) {
+					$scope.folders.splice(index, 1, response);
+				}
+			});
+
+			// Close Edit Folder
+			$scope.toggleEditFolder();
+
+			return response;
+		}, function (e) {
+			znMessage('Error saving folder', 'error');
+		});
+
 	};
 
 	// Initialize for Workspace ID
 	if ($routeParams.workspace_id) {
-			// Set Selected Workspace ID
-			$scope.workspaceId = $routeParams.workspace_id;
+		// Set Selected Workspace ID
+		$scope.workspaceId = $routeParams.workspace_id;
 
-			// Load Workspace Forms
-			$scope.loadForms();
+		// Load Workspace Forms, then Pick First Form
+		$scope.loadForms().then(function() {
+			if ($scope.forms) {
+				$scope.pickForm($scope.forms[0].id);
+			}
+		});
 	}
 
 }])
-
 /**
  * Plugin Registration
  */
